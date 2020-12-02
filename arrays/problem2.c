9cdd4ad95477f784bd38f8f87119e7597e41678b
mandarvu <mandar1695@gmail.com>
2020-12-02 12:46:31 +0530

    Minor bug fix in problem2

--- arrays/problem2.c
+++ arrays/problem2.c
@@ -2,18 +2,26 @@
 #define SIZE 5
 
 int main(int argc, char const *argv[]) {
-  int numbers[SIZE], large, small;
-  printf("Enter %d numbers: ", SIZE);
-  for (int i = 0; i < SIZE; i++)
-    scanf("%d",numbers + i);
-  large = small = *(numbers + 0);
+	int numbers[SIZE], large, small;
+	printf("Enter %d numbers: ", SIZE);
+	for (int i = 0; i < SIZE; i++)
+		scanf("%d",numbers + i);
+	large = *(numbers + 0);
+	small = *(numbers + 0); 
 
-  for (int j = 0; j < SIZE - 1; j++) {
-      if (*(numbers + j) > *(numbers + j + 1))
-        small = *(numbers + j + 1);
-      else
-        large = *(numbers + j + 1);
-  }
+	for (int j = 0; j < SIZE; j++) {
+		if (*(numbers + j) > large)
+			large = *(numbers + j);
+		else
+			continue;
+	}
 
-  printf("Largest %d and smallest %d\n",large, small);
+	for (int j = 0; j < SIZE; j++) {
+		if (*(numbers + j + 1) < small)
+			small = *(numbers + j);
+		else
+			continue;
+	}
+
+	printf("Largest %d and smallest %d\n",large, small);
 }
